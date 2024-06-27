3b7864ce9715d25bde8528caffabc4d1c0171b1a
Himanshu Bhatt <himanshubhatt@Himanshus-MacBook-Air.local>
2024-06-28 00:17:40 +0530

    blocking/stopping feature done

---
+++ url.go
@@ -0,0 +1,21 @@
+package main
+
+// import (
+// 	"fmt"
+
+// 	"github.com/google/uuid"
+// )
+
+// func GreetingH() {
+// 	id := uuid.New()
+// 	fmt.Println("id is ", id)
+// }
+var blockedURLs = []string{
+    "facebook.com",
+    "twitter.com",
+    "instagram.com",
+    "linkedin.com",
+    "snapchat.com",
+    "tiktok.com",
+    "reddit.com",
+}
